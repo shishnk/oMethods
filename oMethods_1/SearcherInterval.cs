cf1b684d2f0ac872b1637e611c3d28cf20790d1e
shishnk <akorund@mail.ru>
2022-02-09 00:29:20 +0700

    Нахождение интервала, содержащего минимум функции

---
+++ oMethods_1/SearcherInterval.cs
@@ -0,0 +1,33 @@
+namespace oMethods_1;
+
+public static class SearcherInterval
+{
+    public static Interval Search(IFunction function, double sigma)
+    {
+        double x0 = 30;
+        double x, xk, xk1, h;
+
+        if (function.Func(x0) > function.Func(x0 + sigma))
+        {
+            xk = x0 + sigma;
+            h = sigma;
+        }
+        else
+        {
+            xk = x0 - sigma;
+            h = -sigma;
+        }
+
+        do
+        {
+            h *= 2;
+            xk1 = xk + h;
+            x = x0;
+            x0 = xk;
+            xk = xk1;
+
+        } while (function.Func(x0) > function.Func(xk));
+
+        return (x < xk1) ? new(x, xk1) : new(xk1, x);
+    }
+}
