5f7e42b1fc3a86a1c0797a2b5577d010438a1bfb
shishnk <akorund@mail.ru>
2022-02-09 00:25:49 +0700

    Клиентский код

---
+++ oMethods_1/Program.cs
@@ -0,0 +1,10 @@
+﻿using oMethods_1;
+
+Solver solver = new("parameters.txt");
+solver.SetFunction(new Function());
+// solver.SetMethod(new Dichotomy());
+// solver.SetMethod(new GoldenSectionSearch());
+solver.SetMethod(new Fibonacci());
+solver.Compute();
+solver.WriteToFile("result.txt");
+Console.WriteLine(SearcherInterval.Search(new Function(), 1E-7));
