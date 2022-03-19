b6df44b76a6703fd30fd6f009bfb796837c11397
shishnk <akorund@mail.ru>
2022-03-19 21:18:18 +0700

    Структура интервала

---
+++ oMethods_2/Interval.cs
@@ -0,0 +1,19 @@
+namespace oMethods_2;
+
+public record struct Interval {
+    public double Left { get; init; }
+    public double Right { get; init; }
+    public double Center { get; init; }
+
+    public Interval(double left, double right) {
+        Left = left;
+        Right = right;
+        Center = (left + right) / 2;
+    }
+
+    public bool Contain(double point)
+        => (point >= Left && point <= Right);
+
+    public override string ToString()
+        => $"[{Left}; {Right}]";
+}
