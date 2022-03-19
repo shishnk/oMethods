50f01e20bc0c186caf3a0297e040de56ba90baf5
shishnk <akorund@mail.ru>
2022-03-19 21:08:44 +0700

    Клиентский код

---
+++ oMethods_2/Program.cs
@@ -0,0 +1,10 @@
+﻿using oMethods_2;
+
+Solver solver = new("point1.json");
+solver.SetFunction(new Function());
+// solver.SetMinMethod(new SimplexMethod(200, 1, 1E-7));
+solver.SetMinMethod(new BFGS(200, 1E-4));
+solver.Compute();
+
+// ScriptEngine engine = Python.CreateEngine(); // IronPython не поддерживает некоторые пакеты Python(к примеру numpy-> грустно :(  ))
+// engine.ExecuteFile("graphics.py");
