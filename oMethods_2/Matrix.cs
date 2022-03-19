54051b48f96e6f3311a4b78c3f529c316509f25f
shishnk <akorund@mail.ru>
2022-03-19 21:19:11 +0700

    Класс матрицы (для BFGS, поэтому надо исправлять)

---
+++ oMethods_2/Matrix.cs
@@ -0,0 +1,51 @@
+namespace oMethods_2;
+
+public class Matrix {
+    private double[,] A;
+
+    public double this[int i, int j] {
+        get => A[i, j];
+        set => A[i, j] = value;
+    }
+
+    public int Size { get; init; }
+
+    public Matrix(int size) {
+        A = new double[size, size];
+        Size = size;
+    }
+
+    public void Clear() {
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+                A[i, j] = 0;
+    }
+
+    public static Point2D operator *(Matrix matrix, double[] vector) {
+        double[] result = new double[matrix.Size];
+
+        for (int i = 0; i < matrix.Size; i++)
+            for (int j = 0; j < matrix.Size; j++)
+                result[i] += matrix[i, j] * vector[j];
+
+        return new(result[0], result[1]);
+    }
+
+    public static Matrix operator /(Matrix matrix, double constant) {
+        for (int i = 0; i < matrix.Size; i++)
+            for (int j = 0; j < matrix.Size; j++)
+                matrix[i, j] /= constant;
+
+        return matrix;
+    }
+
+    public static Matrix operator +(Matrix fstMatrix, Matrix sndMatrix) {
+        Matrix result = new(fstMatrix.Size);
+
+        for (int i = 0; i < result.Size; i++)
+            for (int j = 0; j < result.Size; j++)
+                result[i, j] = fstMatrix[i, j] + sndMatrix[i, j];
+
+        return result;
+    }
+}
