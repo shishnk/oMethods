6cebdc9a0eccbe8ecd98dded74c5c1de5960f43e
shishnk <akorund@mail.ru>
2022-03-20 16:23:00 +0700

    Клиентский код

--- oMethods_2/Program.cs
+++ oMethods_2/Program.cs
@@ -1,9 +1,10 @@
 ﻿using oMethods_2;
 
-Solver solver = new("point1.json");
-solver.SetFunction(new Function());
-// solver.SetMinMethod(new SimplexMethod(200, 1, 1E-7));
-solver.SetMinMethod(new BFGS(200, 1E-4));
+Solver solver = new("initPoint.json");
+solver.SetFunction(new Polynom());
+solver.SetMinMethod(new SimplexMethod(200, 1, 1E-7));
+// solver.SetMinMethod(new BFGS(200, 1E-4));
+// solver.SetMinMethod1D(new Fibonacci(1E-4));
 solver.Compute();
 
 // ScriptEngine engine = Python.CreateEngine(); // IronPython не поддерживает некоторые пакеты Python(к примеру numpy-> грустно :(  ))
