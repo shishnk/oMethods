b5e6dd0da76fc9bb7835e5d5714feea80380ce48
shishnk <akorund@mail.ru>
2022-03-20 21:03:10 +0700

    Переписал BFGS под многомерный случай

--- oMethods_2/BFGS.cs
+++ oMethods_2/BFGS.cs
@@ -1,9 +1,9 @@
 namespace oMethods_2;
 
 public class BFGS : IMinMethodND {
-    private Point2D _min;
-    public Point2D Min
-        => _min;
+    private Argument _min;
+    public Argument Min => _min;
+    public bool Need1DSearch => true;
     public int MaxIters { get; init; }
     public double Eps { get; init; }
 
@@ -12,25 +12,32 @@ public class BFGS : IMinMethodND {
         Eps = eps;
     }
 
-    public void Compute(Point2D initPoint, IFunction function) {
-        Matrix H = new(2);
-        Fibonacci fB = new(Eps);
-        QuadraticInterpolation qI = new(Eps);
-        double[] sum = new double[2];
-        double h = 1E-14;
+    public void Compute(Argument initPoint, IFunction function, IMinMethod1D method) {
+        int n = initPoint.Number;
         double lambda;
         int iters;
-        Point2D direction; // вектор, определяющий направление
-        Point2D nextPoint = new(0, 0);
-        Point2D deltaF;
-        Point2D deltaF1; // производные по n переменным(в нашем случае 2D)
-        Point2D s; // $x_{k+1} - x_{k}$ - шаг алгоритма на итерации
-        Point2D y; // изменение градиента на итерации
+        double h = 1E-14;
+
+        Matrix H = new(n);
+        Matrix H1 = new(n);
+        Argument nextPoint = new(n);
+        Argument direction = new(n); // вектор, определяющий направление
+        Argument deltaF = new(n);
+        Argument deltaF1 = new(n); // производные по n переменным(в нашем случае 2D)
+        Argument s = new(n); // $x_{k+1} - x_{k}$ - шаг алгоритма на итерации
+        Argument y = new(n); // изменение градиента на итерации
+
+        Argument denominatorAsVector;
+        Argument numerator;
+        double denominatorAsNumber;
 
         for (int i = 0; i < H.Size; i++) // начальный гессиан (единичная матрица)
             H[i, i] = 1;
 
         for (iters = 0; iters < MaxIters; iters++) {
+            direction.Fill(0);
+            denominatorAsNumber = 0;
+
             if (iters % 2 == 0 && iters != 0) {
                 H.Clear();
 
@@ -38,75 +45,83 @@ public class BFGS : IMinMethodND {
                     H[i, i] = 1;
             }
 
-            deltaF = new(Derivative(initPoint, function, 0, h), Derivative(initPoint, function, 1, h));
+            for (int i = 0; i < n; i++)
+                deltaF[i] = Derivative(initPoint, function, i, h);
 
             if (Norm(deltaF) < Eps) {
-                _min = initPoint;
+                _min = (Argument)initPoint.Clone();
                 break;
             }
 
             for (int i = 0; i < H.Size; i++)
-                for (int j = 0; j < H.Size; j++) {
-                    sum[i] += -H[i, j] * Derivative(initPoint, function, j, h);
-                }
+                for (int j = 0; j < H.Size; j++)
+                    direction[i] += -H[i, j] * deltaF[j];
 
-            direction = new(sum[0], sum[1]);
-            Array.Clear(sum);
+            method.Compute(SearcherInterval.Search(function, 0, Eps, direction, initPoint), function, initPoint, direction);
+            lambda = method.Min;
+            nextPoint = (Argument)(initPoint + lambda * direction).Clone();
 
-            qI.Compute(SearcherInterval.Search(function, 0, Eps, direction, initPoint), function, initPoint, direction);
-            lambda = qI.Min;
-            nextPoint = initPoint + lambda * direction;
-            deltaF1 = new(Derivative(nextPoint, function, 0, h), Derivative(nextPoint, function, 1, h));
+            for (int i = 0; i < n; i++)
+                deltaF1[i] = Derivative(nextPoint, function, i, h);
 
             y = deltaF1 - deltaF;
             s = nextPoint - initPoint;
 
-            if (s == lambda * direction) {
+            if (s.Equals(lambda * direction)) {
                 H.Clear();
 
                 for (int i = 0; i < H.Size; i++)
                     H[i, i] = 1;
 
-                initPoint = nextPoint;
+                initPoint = (Argument)nextPoint.Clone();
                 continue;
             }
 
+            denominatorAsVector = s - H * y; // вектор в знаменателе соотношения для deltaH
 
-            double[] yArray = new double[2] { y.X, y.Y };
+            for (int i = 0; i < n; i++)
+                denominatorAsNumber += denominatorAsVector[i] * y[i]; // подсчитанный знаменатель
 
-            Matrix H1 = new(H.Size);
+            numerator = (Argument)denominatorAsVector.Clone();
 
-            Point2D numerator = s - H * yArray;
-            H1[0, 0] = numerator.X * numerator.X;
-            H1[0, 1] = numerator.X * numerator.Y;
-            H1[1, 0] = numerator.Y * numerator.X;
-            H1[1, 1] = numerator.Y * numerator.Y;
-            Point2D denominator = s - H * yArray;
-            double denominator1 = denominator.X * y.X + denominator.Y * y.Y;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    H1[i, j] = numerator[i] * numerator[j];
 
-            if (Math.Abs(denominator1) < Eps) {
+            if (Math.Abs(denominatorAsNumber) < Eps) {
                 H.Clear();
 
                 for (int i = 0; i < H.Size; i++)
                     H[i, i] = 1;
 
-                initPoint = nextPoint;
+                initPoint = (Argument)nextPoint.Clone();
                 continue;
             }
 
-            H1 /= denominator1;
+            H1 /= denominatorAsNumber;
             H += H1;
-            initPoint = nextPoint;
+            initPoint = (Argument)nextPoint.Clone();
         }
 
         Console.WriteLine($"Iterations: {iters}");
         Console.WriteLine(JsonConvert.SerializeObject(_min));
-        Console.WriteLine($"f({_min.X}; {_min.Y}) = {function.Value(_min)}");
+        Console.WriteLine($"f(min) = {function.Value(_min)}");
     }
-    private double Norm(Point2D point)
-        => Math.Sqrt(point.X * point.X + point.Y * point.Y);
 
-    private double Derivative(Point2D point, IFunction function, int numberVariable, double h)
-        => (numberVariable == 0) ? (function.Value(point + (h, 0)) - function.Value(point - (h, 0))) / (2 * h) :
-           (function.Value(point + (0, h)) - function.Value(point - (0, h))) / (2 * h);
+    private double Norm(Argument arg) {
+        double result = 0;
+
+        for (int i = 0; i < arg.Number; i++) {
+            result += arg[i] * arg[i];
+        }
+
+        return Math.Sqrt(result);
+    }
+
+    private double Derivative(Argument point, IFunction function, int numberVariable, double h) {
+        Argument arg = new(point.Number);
+        arg[numberVariable] = h;
+
+        return (function.Value(point + arg) - function.Value(point - arg)) / (2 * h);
+    }
 }
