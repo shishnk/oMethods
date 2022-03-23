71e51926e0d872f6a86e396849c0c5c9449e94c4
shishnk <akorund@mail.ru>
2022-03-23 23:27:43 +0700

    Переписал чтение и запись json

--- oMethods_2/Argument.cs
+++ oMethods_2/Argument.cs
@@ -1,6 +1,39 @@
 namespace oMethods_2;
 
+public class ArgumentConverter : JsonConverter {
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+        Argument arg = (Argument)value;
+
+        writer.WriteStartObject();
+        writer.WritePropertyName("Extremum");
+        writer.WriteStartArray();
+        Array.ForEach(arg.Variables, (x) => writer.WriteValue(x));
+        writer.WriteEndArray();
+        writer.WriteEndObject();
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+        Argument value;
+        
+        var maintoken = JObject.Load(reader);
+        var token = maintoken["Number"];
+        int number = Convert.ToInt32(token);
+
+        value = new(number);
+
+        token = maintoken["Point"];
+        value.Variables = JsonConvert.DeserializeObject<double[]>(token.ToString());
+        
+        return value;
+    }
+
+    public override bool CanConvert(Type objectType)
+        => objectType == typeof(Argument);
+}
+
+[JsonConverter(typeof(ArgumentConverter))]
 public class Argument : ICloneable {
+    [JsonProperty("Point")]
     public double[] Variables { get; set; }
     public int Number { get; init; }
 
