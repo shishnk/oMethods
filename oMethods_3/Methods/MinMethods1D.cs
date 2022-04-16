27df6ba75a18d10c03d3d86ab1e6cd163212f92d
shishnk <akorund@mail.ru>
2022-04-16 23:23:56 +0700

    Методы одномерного поиска (взяты с прошлой лабораторной + внесены поправки из-за nullable context)

---
+++ oMethods_3/Methods/MinMethods1D.cs
@@ -0,0 +1,90 @@
+namespace oMethods_3;
+
+public interface IMinMethod1D {
+    public double? Min { get; }
+    public double Eps { get; init; }
+
+    public void Compute(Interval interval, IFunction function, Argument argument, Argument direction);
+}
+
+public class Fibonacci : IMinMethod1D {
+    private double? _min;
+    public double? Min => _min;
+    public double Eps { get; init; }
+
+    public Fibonacci(double eps)
+        => Eps = eps;
+
+    public void Compute(Interval interval, IFunction function, Argument argument, Argument direction) {
+        int n = 1;
+
+        while ((interval.Right - interval.Left) / Eps > Fib(n + 2))
+            n++;
+
+        double x1 = interval.Left + Fib(n) / Fib(n + 2) * (interval.Right - interval.Left);
+        double x2 = interval.Left + Fib(n + 1) / Fib(n + 2) * (interval.Right - interval.Left);
+        double y1 = function.Value(argument + x1 * direction);
+        double y2 = function.Value(argument + x2 * direction);
+
+        for (int k = 1; k < n; k++) {
+            if (y1 < y2) {
+                interval = new(interval.Left, x2);
+                x2 = x1;
+                y2 = y1;
+                x1 = interval.Left + Fib(n - k + 1) / Fib(n - k + 3) * (interval.Right - interval.Left);
+                y1 = function.Value(argument + x1 * direction);
+            } else {
+                interval = new(x1, interval.Right);
+                x1 = x2;
+                y1 = y2;
+                x2 = interval.Left + Fib(n - k + 2) / Fib(n - k + 3) * (interval.Right - interval.Left);
+                y2 = function.Value(argument + x2 * direction);
+            }
+        }
+
+        _min = interval.Center;
+    }
+
+    private static double Fib(int n)
+        => (Math.Pow((1 + Math.Sqrt(5)) / 2, n) -
+            Math.Pow((1 - Math.Sqrt(5)) / 2, n)) / Math.Sqrt(5);
+}
+
+public class QuadraticInterpolation : IMinMethod1D {
+    private double? _min;
+    public double? Min => _min;
+    public double Eps { get; init; }
+
+    public QuadraticInterpolation(double eps)
+        => Eps = eps;
+
+    public void Compute(Interval interval, IFunction function, Argument argument, Argument direction) {
+        double xk, x1, x2, b, c;
+        int iters;
+        double x0 = interval.Center;
+        double step = (interval.Right - interval.Left) / 2;
+
+        for (iters = 0; ; iters++) {
+            x1 = x0 - step;
+            x2 = x0 + step;
+
+            c = (function.Value(argument + x1 * direction) -
+                2 * function.Value(argument + x0 * direction) +
+                function.Value(argument + x2 * direction)) /
+                (2 * step * step);
+
+            b = (-function.Value(argument + x1 * direction) *
+                (2 * x0 + step) + 4 * function.Value(argument + x0 * direction) *
+                x0 - function.Value(argument + x2 * direction) *
+                (2 * x0 - step)) / (2 * step * step);
+
+            xk = -b / (2 * c);
+
+            if (Math.Abs(xk - x0) < Eps) {
+                _min = xk;
+                break;
+            } else
+                x0 = xk;
+        }
+    }
+}
