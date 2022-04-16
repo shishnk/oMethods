1ca16b9f461bfc668ab39f8443b7db0d859d3fa6
shishnk <akorund@mail.ru>
2022-04-16 23:18:56 +0700

    Клиентский код

---
+++ oMethods_3/Program.cs
@@ -0,0 +1,4 @@
+﻿using oMethods_3;
+
+// Solver solver = Solver.CreateBuilder().SetPoint(Argument.ReadJson("point.json")??).SetFunction().SetMinMethod().SetMethod().SetPenaltyCoef();
+// Solver solver = Solver.CreateBuilder().SetPoint(Argument.ReadJson("point.json")??).SetFunction().SetMinMethod().SetMinMethod1D();
