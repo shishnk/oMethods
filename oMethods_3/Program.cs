7e710f5669c3e64260022c672f4e6db8b6bbe644
shishnk <akorund@mail.ru>
2022-04-17 18:58:26 +0700

    Клиентский код

--- oMethods_3/Program.cs
+++ oMethods_3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using oMethods_3;
 
-// Solver solver = Solver.CreateBuilder().SetPoint(Argument.ReadJson("point.json")??).SetFunction().SetMinMethod().SetMethod().SetPenaltyCoef();
-// Solver solver = Solver.CreateBuilder().SetPoint(Argument.ReadJson("point.json")??).SetFunction().SetMinMethod().SetMinMethod1D();
+Solver solver = Solver.CreateBuilder().SetPoint(Argument.ReadJson("point.json")!).
+SetFunction(new FunctionA(2, 1)).SetMinMethod(new GaussAlgorithm(10000, 1E-03)).
+SetMinMethod1D(new Fibonacci(1E-07)).SetStrategyType(StrategyTypes.Multiply);
+
+solver.Compute();
