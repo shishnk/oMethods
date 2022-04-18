fa55b0af58179d5dad8c274dbcbd2325937ce7c9
shishnk <akorund@mail.ru>
2022-04-18 18:52:33 +0700

    Добавил статичную переменную также для кол-ва вызова функции (добавлена только для отчета)

--- oMethods_3/SearcherInterval.cs
+++ oMethods_3/SearcherInterval.cs
@@ -1,11 +1,16 @@
 namespace oMethods_3;
 
 public static class SearcherInterval {
+    // for report
+    public static int Call = 0;
+    //
     public static Interval Search(IFunction function, double x0, double delta, Argument direction, Argument argument) {
         double x, xk, xk1, h;
 
-        if (function.Value(argument + x0 * direction) + function.PenaltyValue(argument + x0 * direction) > 
+        if (function.Value(argument + x0 * direction) + function.PenaltyValue(argument + x0 * direction) >
             function.Value(argument + (x0 + delta) * direction) + function.PenaltyValue(argument + (x0 + delta) * direction)) {
+
+            Call += 2;
 
             xk = x0 + delta;
             h = delta;
@@ -21,7 +26,9 @@ public static class SearcherInterval {
             x0 = xk;
             xk = xk1;
 
-        } while (function.Value(argument + x0 * direction) + function.PenaltyValue(argument + x0 * direction) > 
+            Call += 2;
+
+        } while (function.Value(argument + x0 * direction) + function.PenaltyValue(argument + x0 * direction) >
                  function.Value(argument + xk * direction) + function.PenaltyValue(argument + xk * direction));
 
         return (x < xk1) ? new(x, xk1) : new(xk1, x);
