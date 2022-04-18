e6295c33212e27396074b0b1052102f62e61d187
shishnk <akorund@mail.ru>
2022-04-18 10:12:56 +0700

    Добавил барьерные функции

--- oMethods_3/Functions/Functions.cs
+++ oMethods_3/Functions/Functions.cs
@@ -2,7 +2,8 @@ namespace oMethods_3;
 
 public interface IFunction {
     public double Coef { get; set; }
-    public int Degree { get; init; }
+    public int? Degree { get; init; }
+    public MethodTypes? MethodType { get; init; }
     public double Value(Argument arg);
     public double PenaltyValue(Argument arg);
     public bool Limitation(Argument arg);
@@ -10,17 +11,25 @@ public interface IFunction {
 
 public class FunctionA : IFunction {
     public double Coef { get; set; }
-    public int Degree { get; init; }
+    public int? Degree { get; init; }
+    public MethodTypes? MethodType { get; init; }
 
-    public FunctionA(double? coef, int degree)
-        => (Coef, Degree) = (coef ?? 0, degree);
+    public FunctionA(double? coef, int? degree, MethodTypes? methodType)
+        => (Coef, Degree, MethodType) = (coef ?? 0, degree, methodType ?? MethodTypes.Penalty);
 
     public double Value(Argument arg)
         => 5 * (arg[0] + arg[1]) * (arg[0] + arg[1]) +
            (arg[0] - 2) * (arg[0] - 2);
 
-    public double PenaltyValue(Argument arg)
-        => Math.Pow(0.5 * (-arg[0] - arg[1] + 1 + Math.Abs(-arg[0] - arg[1] + 1)), Degree) / Coef;
+    public double PenaltyValue(Argument arg) => MethodType switch {
+        MethodTypes.Penalty => Coef * Math.Pow(0.5 * (-arg[0] - arg[1] + 1 + Math.Abs(-arg[0] - arg[1] + 1)), Degree!.Value),
+
+        MethodTypes.InteriorPointLog => -Coef * Math.Log(arg[0] + arg[1] - 1),
+
+        // MethodTypes.InteriorPointReverse =>  Coef / (arg[0] + arg[1] - 1), // TODO
+
+        _ => throw new InvalidEnumArgumentException($"This type of method does not exist: {nameof(MethodType)}")
+    };
 
     public bool Limitation(Argument arg)
         => -arg[0] - arg[1] + 1 < 0;
@@ -28,18 +37,18 @@ public class FunctionA : IFunction {
 
 public class FunctionB : IFunction {
     public double Coef { get; set; }
+    public int? Degree { get; init; }
+    public MethodTypes? MethodType { get; init; }
 
-    public int Degree { get; init; }
-
-    public FunctionB(double? coef, int degree)
-        => (Coef, Degree) = (coef ?? 0, degree);
+    public FunctionB(double? coef, int? degree, MethodTypes? methodType)
+        => (Coef, Degree, MethodType) = (coef ?? 0, degree, methodType ?? MethodTypes.Penalty);
 
     public double Value(Argument arg)
         => 5 * (arg[0] + arg[1]) * (arg[0] + arg[1]) +
            (arg[0] - 2) * (arg[0] - 2);
 
     public double PenaltyValue(Argument arg)
-         => Coef * Math.Pow(Math.Abs(arg[0] - arg[1]), Degree);
+         => Coef * Math.Pow(Math.Abs(arg[0] - arg[1]), Degree!.Value);
 
     public bool Limitation(Argument arg)
         => Math.Abs(arg[0] - arg[1]) < 1E-12;
