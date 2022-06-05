16273b64e862070344e4796f633974d7dab6044c
shishnk <akorund@mail.ru>
2022-06-06 02:42:04 +0700

    Рабочий алгоритм случайного поиска

--- oMethods_4/SimpleRandomSearch.cs
+++ oMethods_4/SimpleRandomSearch.cs
@@ -4,33 +4,37 @@ public class SimpleRandomSearch : IMinMethod2D {
     private Vector2D? _min;
     public Vector2D? Min => _min;
     public double Eps { get; init; }
+    public double Probability { get; init; }
 
-    public SimpleRandomSearch(double eps)
-        => Eps = eps;
+    public SimpleRandomSearch(double eps, double probability)
+        => (Eps, Probability) = (eps, probability);
 
     public void Compute(Rectangle rectangle, IFunction function) {
-        double initValue = function.Value(rectangle.LeftBottom);
+        double functionMaxValue = function.Value(rectangle.LeftBottom);
 
         int tests = 1;
         double neighbourhood = Eps * Eps;
         double probabilityEps = neighbourhood / rectangle.Square;
-        double probabilityN = 1.0 - Math.Pow(1.0 - probabilityEps, tests);
 
-        // while (tests <= Math.Log(1.0 - probabilityN) / Math.Log(1.0 - probabilityEps)) {
-        //     tests++;
-        //     probabilityN = 1.0 - Math.Pow(1.0 - probabilityEps, tests);
-        // }
+        while (tests < Math.Log(1.0 - Probability) / Math.Log(1.0 - probabilityEps)) {
+            tests++;
+        }
 
-        // for (int itest = 0; itest < tests; itest++) {
-        //     double newX = new Random().NextDouble(rectangle.LeftBottom.X, rectangle.RightBottom.X);
-        //     double newY = new Random().NextDouble(rectangle.LeftBottom.Y, rectangle.LeftTop.Y);
+        for (int itest = 0; itest < tests; itest++) {
+            double newX = new Random().NextDouble(rectangle.LeftBottom.X, rectangle.RightBottom.X);
+            double newY = new Random().NextDouble(rectangle.LeftBottom.Y, rectangle.LeftTop.Y);
 
-        //     if (function.Value(new(newX, newY)) > initValue) {
-        //         _min = new(newX, newY);
-        //     }
-        // }
+            double temp;
 
-        Console.WriteLine($"Extremum: {_min = new(-2.89, 7.08)}");
-        Console.WriteLine($"f(extremum) = {function.Value(_min!.Value)}");
+            if ((temp = function.Value(new(newX, newY))) > functionMaxValue) {
+                _min = new(newX, newY);
+                functionMaxValue = temp;
+            }
+        }
+
+        Console.WriteLine($"N: {tests}");
+        Console.WriteLine($"P: {Probability}");
+        Console.WriteLine($"Extremum: {_min}");
+        Console.WriteLine($"f(extremum) = {functionMaxValue}");
     }
 }
