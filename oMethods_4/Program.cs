d56c5533c6a40e4b06d4611b30642acade395f1f
shishnk <akorund@mail.ru>
2022-06-06 02:50:43 +0700

    Внес поправки в клиентский код

--- oMethods_4/Program.cs
+++ oMethods_4/Program.cs
@@ -2,7 +2,7 @@
 
 Solver solver = Solver.CreateBuilder()
 .SetFunction(new Function())
-.SetMethod(new SimpleRandomSearch(1E-10))
+.SetMethod(new SimpleRandomSearch(0.1, 0.999))
 .SetArea(Rectangle.ReadJson("area.json")!.Value);
 
 solver.Compute();
