39d1d25174a590eaa79495d3c209bbd1e57f8cc8
shishnk <akorund@mail.ru>
2022-06-06 19:13:15 +0700

    Интерфейсы

--- oMethods_4/IMethods.cs
+++ oMethods_4/IMethods.cs
@@ -4,5 +4,12 @@ public interface IMinMethod2D {
     public Vector2D? Min { get; }
     public double Eps { get; init; }
 
+    public void Compute(IFunction function, Vector2D initPoint);
+}
+
+public interface ISearchMethod2D {
+    public Vector2D? Min { get; }
+    public double Eps { get; init; }
+
     public void Compute(Rectangle rectangle, IFunction function);
 }
