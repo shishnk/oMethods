a4ee19186b6197b11fa62d987025de7e7aa30c72
shishnk <akorund@mail.ru>
2022-06-06 19:21:42 +0700

    Внес поправки

--- oMethods_4/Function.cs
+++ oMethods_4/Function.cs
@@ -6,10 +6,10 @@ public interface IFunction {
 
 public class Function : IFunction {
     public double Value(Vector2D point)
-        => (2.0 / (1 + ((point.X - 5) * (point.X - 5)) + ((point.Y - 4) * (point.Y - 4)))) +
+        => -((2.0 / (1 + ((point.X - 5) * (point.X - 5)) + ((point.Y - 4) * (point.Y - 4)))) +
            (1.0 / (1 + ((point.X - 2) * (point.X - 2)) + (point.Y * point.Y))) +
            (7.0 / (1 + ((point.X + 9) * (point.X + 9)) + ((point.Y + 6) * (point.Y + 6)))) +
            (2.0 / (1 + (point.X * point.X) + ((point.Y + 3) * (point.Y + 3)))) +
            (8.0 / (1 + ((point.X + 3) * (point.X + 3)) + ((point.Y - 7) * (point.Y - 7)))) +
-           (4.0 / (1 + ((point.X + 3) * (point.X + 3)) + ((point.Y - 3) * (point.Y - 3))));
+           (4.0 / (1 + ((point.X + 3) * (point.X + 3)) + ((point.Y - 3) * (point.Y - 3)))));
 }
